9b4044dfa18736b6e49f0107018c97e95ecec606
Wesley Stam <wesley@stam.me>
2013-03-24 22:25:04 +0100

    ignore existing files

--- setup.rb
+++ setup.rb
@@ -2,5 +2,7 @@
 
 Dir.entries('.').each do |filename|
   next if filename[0] == 46 || __FILE__ == filename || filename == "README"
-  File.symlink("#{File.expand_path(".")}/#{filename}", "#{File.expand_path("~")}/.#{filename}")
+  unless File.exist?("#{File.expand_path("~")}/.#{filename}")
+    File.symlink("#{File.expand_path(".")}/#{filename}", "#{File.expand_path("~")}/.#{filename}")
+  end
 end
