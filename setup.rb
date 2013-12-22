a34559999895c9e01116599d283c6b5125f059d1
Wesley Stam <wesley@stam.me>
2013-12-22 20:44:16 +0100

    ignore dotfiles in setup

--- setup.rb
+++ setup.rb
@@ -1,7 +1,8 @@
-#!/usr/bin/ruby -w
+#!/usr/bin/ruby
 
-Dir.entries('.').each do |filename|
-  next if filename[0] == 46 || __FILE__ == filename || filename == "README"
+Dir.entries(".").each do |filename|
+  next if filename[0] == 46 || __FILE__ == filename || filename == "README" || filename[0] == '.'
+
   unless File.exist?("#{File.expand_path("~")}/.#{filename}")
     File.symlink("#{File.expand_path(".")}/#{filename}", "#{File.expand_path("~")}/.#{filename}")
   end
