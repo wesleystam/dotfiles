4524f6f4b1319ac3d67d077cfe5dc8c2db436a81
Wesley Stam <wesley@stam.me>
2024-01-16 15:06:52 +0100

    Remove default gitignore

--- setup.rb
+++ setup.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-%w[vim vimrc gitignore gitconfig gitattributes gemrc irbrc railsrc zshrc].each do |filename|
+%w[vim vimrc gitconfig gitattributes gemrc irbrc railsrc zshrc].each do |filename|
   unless File.exist?("#{Dir.home}/.#{filename}")
     File.symlink("#{Dir.home}/dotfiles/#{filename}", "#{Dir.home}/.#{filename}")
   end
